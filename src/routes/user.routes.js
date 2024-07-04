b0a7d3763df6af01b62f64de6c163f8968ae04e8
abhradeepbarman <abhradeep709@gmail.com>
2024-07-04 23:04:17 +0530

    routes for user controllers added

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const { register, login, logout, refreshAccessToken } = require("../controllers/user.controller.js")
+const { register, login, logout, refreshAccessToken, changeCurrentPassword, getCurrentuser, updateAccountDetails, updateUserAvatar, updateUserCoverImage, getUserChannelProfile, getWatchHistory } = require("../controllers/user.controller.js")
 const { upload } = require("../middlewares/multer.middleware.js")
 const { verifyJWT } = require("../middlewares/auth.middleware.js")
 const router = express.Router()
@@ -14,11 +14,20 @@ router.post("/register", upload.fields([
         maxCount: 1
     }
 ]), register)
-
 router.post("/login", login)
 
 //secured routes
 router.post("/logout", verifyJWT, logout)
 router.post("/refresh-token", refreshAccessToken)
+router.post("/change-password", verifyJWT, changeCurrentPassword)
+router.get("/current-user", verifyJWT, getCurrentuser)
+router.patch("/update-account-details", verifyJWT, updateAccountDetails)
+
+router.patch("/update-avatar", verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.patch("/update-cover-image", verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+
+router.get("/channel/:username", verifyJWT, getUserChannelProfile)
+router.get("/watch-history", verifyJWT, getWatchHistory)
+
 
 module.exports = router
